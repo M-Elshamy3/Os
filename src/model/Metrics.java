908a0e731244563dc6b6e5eba97fe92914abcfe1
M-Elshamy3 <mazenashraf2666@gmail.com>
2026-05-04 12:35:50 +0300

    Refactor Metrics class to a record

--- src/model/Metrics.java
+++ src/model/Metrics.java
@@ -1,25 +1,4 @@
 package model;
 
-public class Metrics {
-    private final double avgWT;
-    private final double avgTAT;
-    private final double avgRT;
-
-    public Metrics(double avgWT, double avgTAT, double avgRT) {
-        this.avgWT = avgWT;
-        this.avgTAT = avgTAT;
-        this.avgRT = avgRT;
-    }
-
-    public double getAvgWT() {
-        return avgWT;
-    }
-
-    public double getAvgTAT() {
-        return avgTAT;
-    }
-
-    public double getAvgRT() {
-        return avgRT;
-    }
+public record Metrics(double avgWT, double avgTAT, double avgRT) {
 }
