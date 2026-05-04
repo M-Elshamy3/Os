fd196f0b6604af91379cef6562d1a480269c79b7
M-Elshamy3 <mazenashraf2666@gmail.com>
2026-05-04 12:34:08 +0300

    Refactor Process class to a record

--- src/model/Process.java
+++ src/model/Process.java
@@ -1,37 +1,4 @@
 package model;
 
-public class Process {
-    private final String pid;
-    private final int arrival;
-    private final int burst;
-    private final int priority;
-    private final int inputOrder;
-
-    public Process(String pid, int arrival, int burst, int priority, int inputOrder) {
-        this.pid = pid;
-        this.arrival = arrival;
-        this.burst = burst;
-        this.priority = priority;
-        this.inputOrder = inputOrder;
-    }
-
-    public String getPid() {
-        return pid;
-    }
-
-    public int getArrival() {
-        return arrival;
-    }
-
-    public int getBurst() {
-        return burst;
-    }
-
-    public int getPriority() {
-        return priority;
-    }
-
-    public int getInputOrder() {
-        return inputOrder;
-    }
+public record Process(String pid, int arrival, int burst, int priority, int inputOrder) {
 }
