6722dd3de406c41e378bb2db2997d4ee1bcdd91a
M-Elshamy3 <mazenashraf2666@gmail.com>
2026-05-05 02:26:41 +0300

    Refactor cell value factories to use getter methods

--- src/ui/TableFactory.java
+++ src/ui/TableFactory.java
@@ -13,18 +13,19 @@ public class TableFactory {
         TableView<Process> table = new TableView<>();
 
         TableColumn<Process, String> pid = new TableColumn<>("PID");
-        pid.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().pid()));
+        pid.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getPid()));
 
         TableColumn<Process, Number> arrival = new TableColumn<>("Arrival");
-        arrival.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().arrival()));
+        arrival.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getArrival()));
 
         TableColumn<Process, Number> burst = new TableColumn<>("Burst");
-        burst.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().burst()));
+        burst.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getBurst()));
 
         TableColumn<Process, Number> priority = new TableColumn<>("Priority");
-        priority.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().priority()));
+        priority.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getPriority()));
 
         table.getColumns().addAll(pid, arrival, burst, priority);
+
         return table;
     }
 
@@ -32,33 +33,34 @@ public class TableFactory {
         TableView<ResultRow> table = new TableView<>();
 
         TableColumn<ResultRow, String> pid = new TableColumn<>("PID");
-        pid.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().pid()));
+        pid.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getPid()));
 
         TableColumn<ResultRow, Number> at = new TableColumn<>("AT");
-        at.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().arrival()));
+        at.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getArrival()));
 
         TableColumn<ResultRow, Number> bt = new TableColumn<>("BT");
-        bt.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().burst()));
+        bt.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getBurst()));
 
         TableColumn<ResultRow, Number> pr = new TableColumn<>("PR");
-        pr.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().priority()));
+        pr.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getPriority()));
 
         TableColumn<ResultRow, Number> st = new TableColumn<>("ST");
-        st.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().start()));
+        st.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getStart()));
 
         TableColumn<ResultRow, Number> ct = new TableColumn<>("CT");
-        ct.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().completion()));
+        ct.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getCompletion()));
 
         TableColumn<ResultRow, Number> wt = new TableColumn<>("WT");
-        wt.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().waiting()));
+        wt.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getWaiting()));
 
         TableColumn<ResultRow, Number> tat = new TableColumn<>("TAT");
-        tat.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().turnaround()));
+        tat.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getTurnaround()));
 
         TableColumn<ResultRow, Number> rt = new TableColumn<>("RT");
-        rt.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().response()));
+        rt.setCellValueFactory(data -> new SimpleIntegerProperty(data.getValue().getResponse()));
 
         table.getColumns().addAll(pid, at, bt, pr, st, ct, wt, tat, rt);
+
         return table;
     }
 }
